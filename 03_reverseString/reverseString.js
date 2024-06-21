7aa4f5e3a59fe12001a6ab43d3cc74ba22d799f2
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2024-06-20 23:29:07 -0700

    wrote a function that returns any string reversed in three different ways

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
 const reverseString = function (string) {
-    return string.split("").reverse().join("");
+    var myString = "";
+    
+    for(var i=string.length-1; i >= 0; i--){
+        myString += string[i]
+    }
+    return myString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+/*  return string.split("").reverse().join(""); 
+
+    splitStr = string.split("");
+
+    reverseStr = splitStr.reverse();
+
+    joinStr = reverseStr.join("");
+
+    return joinStr;
+*/
