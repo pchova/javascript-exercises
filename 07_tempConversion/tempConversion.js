742d42f956266c6d3ff75ab9712a07f9169c2ac3
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2024-06-26 00:01:50 -0700

    tempConversion passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  let celsius = (temp - 32) * (5/9);
+
+  if (celsius !== 0) {
+    return parseFloat(celsius.toFixed(1));
+  } else {
+    return celsius;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  let fahrenheit = (temp) * (9/5) + 32;
+
+  if (fahrenheit !== 0) {
+    return parseFloat(fahrenheit.toFixed(1));
+  } else {
+    return fahrenheit;
+  }
 };
 
 // Do not edit below this line
