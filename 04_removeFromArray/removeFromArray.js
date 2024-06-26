1761c618cec65e304ab29c0f891a9988896a5851
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2024-06-25 23:38:42 -0700

    leapYears.js passed all tests only

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, arg) {
+    const flatArray = array.flat();
+    
+    const newArray = flatArray.filter((flatArray) => flatArray !== arg);
+     
+    return newArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    let total = 0;
+    for (let i=x; i <= y; i++){
+        total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
