8a441462781e28b9c3d9dddbb3f1e3596a3b66f5
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2024-06-26 23:03:00 -0700

    all tests passed for sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,25 @@
 const sumAll = function(x,y) {
+
     let total = 0;
-    for (let i=x; i <= y; i++){
-        total += i;
-  }
+
+    if (x < 0 || y < 0) {
+      return 'ERROR';
+    } else if (typeof x != 'number' || typeof y != 'number') {
+      return 'ERROR';
+    } else if (x > y ) {
+      let start = y;
+      let end = x;
+      for (let i = start; i <= end; i++) {
+          total += i;
+      }
+    } else if (x < y) {
+      for (let i=x; i <= y; i++){
+        total += i; 
+      }
+    } else {
+      return 'ERROR';
+    }
+    
   return total;
 };
 
